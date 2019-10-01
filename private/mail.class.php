6c5cb9f0ead658d59c37aeacff3269126007b014
Christoph Dunkake <christoph@dragoneyes.org>
2019-10-01 16:32:33 +0200

    mailservice basically working

--- private/mail.class.php
+++ private/mail.class.php
@@ -38,7 +38,7 @@ class Mail {
         curl_setopt_array($curl, array(
             CURLOPT_URL => API_MAIL_URLBASE . "/sessions/" . $apikey,
             CURLOPT_CUSTOMREQUEST => "DELETE",
-            // CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => array(
                 "Content-Type: application/json"
             ),
@@ -49,13 +49,74 @@ class Mail {
         if ($err) {
             echo "cURL Error #:" . $err;
         }
+        return;
     }
 
     public function send() {
         $apikey = $this->apiConnectionOpen();
+
+//        echo $apikey;
+//        echo '<pre>';
+//        print_r($this);
+//        echo '</pre>';
+
         // create record
+        $curl = curl_init();
+        $bodytext = str_replace("\n", "\\n", $this->body);
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://intra.dragoneyes.solutions/fmi/data/v1/databases/CUBApost/layouts/mail/records",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+//            CURLOPT_POSTFIELDS => "{\"fieldData\": \n  { \n  \t\"Recipient\": \"$this->recipient\",\n  \t\"Subject\": \"$this->subject\",\n  \t\"Body\": \"some\\nbody\"\n  } \n}",
+//            CURLOPT_POSTFIELDS => "{\"fieldData\": \n  { \n  \t\"Recipient\": \"$this->recipient\",\n  \t\"Subject\": \"$this->subject\",\n  \t\"Body\": \"$bodytext\"\n  } \n}",
+            CURLOPT_POSTFIELDS => "{\"fieldData\": {\"Recipient\": \"$this->recipient\", \"Subject\": \"$this->subject\", \"Body\": \"$bodytext\" } }",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "cache-control: no-cache",
+                "Authorization: Bearer $apikey"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+//            echo $response;
+        }
+
+        // TODO: only if ok message from creation
         // trigger send-script
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://intra.dragoneyes.solutions/fmi/data/v1/databases/CUBApost/layouts/mail/script/send_mail",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "cache-control: no-cache",
+                "Authorization: Bearer $apikey"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+//            echo $response;
+        }
+
         $this->apiConnectionClose($apikey);
-        exit();
+//        exit();
     }
 }
--- private/meta.php
+++ private/meta.php
@@ -1,5 +1,5 @@
 <?php
     define("REQX_VERSION", '1.5.0 beta');
     define("REQX_RELEASENO", '92');
-    define("REQX_RELEASENO_MAILSERVICE", '2');
-    define("REQX_RELEASEDATE", '2019-09-24');
+    define("REQX_RELEASENO_MAILSERVICE", '4');
+    define("REQX_RELEASEDATE", '2019-10-01');
