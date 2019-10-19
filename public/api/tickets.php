805427566f7d5301fce67d8007aba57156598256
Christoph Dunkake <christoph@dragoneyes.org>
2019-10-19 10:26:40 +0200

    json for all tickets

--- public/api/tickets.php
+++ public/api/tickets.php
@@ -1,19 +1,38 @@
 <?php require_once('../../private/initialize.php');
 
-    header('Access-Control-Allow: *');
+//    header('Access-Control-Allow: *');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: GET');
+//    header('Access-Control-Allow-Methods: GET');
 
     // return all tickets
-
-
-
-    $return = array(
-        "success" => true,
-        "response" => array(
-            "name" => 'Georg',
-            "age" => 34
+    $request_set = find_all_requests();
+    $request = mysqli_fetch_assoc($request_set);
+    $tickets = array(
+        array(
+            "id" => $request['kp_request'],
+            "description" => $request['description'],
+            "category" => find_selectiontext_by_kp(h($request['category'])),
+            "priority" => find_selectiontext_by_kp(h($request['priority'])),
+            "source" => find_userabbr_by_kp(h($request['source'])),
+            "status" => find_selectiontext_by_kp(h($request['status'])),
+            "responsible" => find_userabbr_by_kp(h($request['responsible']))
         )
     );
+    while($request = mysqli_fetch_assoc($request_set)) {
+        $tickets[] = array(
+            "id" => $request['kp_request'],
+            "description" => $request['description'],
+            "category" => find_selectiontext_by_kp(h($request['category'])),
+            "priority" => find_selectiontext_by_kp(h($request['priority'])),
+            "source" => find_userabbr_by_kp(h($request['source'])),
+            "status" => find_selectiontext_by_kp(h($request['status'])),
+            "responsible" => find_userabbr_by_kp(h($request['responsible']))
+        );
+    }
 
-    echo json_encode($return);
+    $response = array(
+        "success" => true,
+        "tickets" => $tickets
+    );
+
+    echo json_encode($response);
